76a1b9edb8889c592b572a68a359cd6ed4c32bf8
HTDBarsi <57287841+HTDBarsi@users.noreply.github.com>
2022-03-03 20:00:09 +0100

    Update lean.lua

--- lean.lua
+++ lean.lua
@@ -1,10 +1,8 @@
 getgenv().LEAN = {}
-local lean = 'abcdefghijklmnopqrstuvwxyz'
-local _lean = {"😆","🙂","🤗","😱","🤢","🙄","🗿","🥺","😒","👿","👍","🎂","💀","🤤","🤮","😷","👽","🐒","🎈","🎗","🎟","💨","💞","💜","🛑","💥"}
+local lean,_lean = 'abcdefghijklmnopqrstuvwxyz',{"😆","🙂","🤗","😱","🤢","🙄","🗿","🥺","😒","👿","👍","🎂","💀","🤤","🤮","😷","👽","🐒","🎈","🎗","🎟","💨","💞","💜","🛑","💥"}
 LEAN.run = function(lean_)
     for i = 1,#lean,1 do
         lean_ = string.gsub(lean_,"LEAN".._lean[i],string.sub(lean,i,i))
     end
-    --print(lean_)
     loadstring(lean_)()
 end
