ab33079f3f28f62644ff14b3d549b37282b84504
HTDBarsi <57287841+HTDBarsi@users.noreply.github.com>
2022-03-03 19:44:01 +0100

    Create lean.lua

---
+++ lean.lua
@@ -0,0 +1,10 @@
+local lean = 'abcdefghijklmnopqrstuvwxyz'
+local _lean = {"😆","🙂","🤗","😱","🤢","🙄","🗿","🥺","😒","👿","👍","🎂","💀","🤤","🤮","😷","👽","🐒","🎈","🎗","🎟","💨","💞","💜","🛑","💥"}
+local run = function(lean_)
+    for i = 1,#lean,1 do
+        code = string.gsub(lean_,"LEAN".._lean[i],string.sub(lean,i,i))
+    end
+    loadstring(lean_)()
+end
+
+return {run}
