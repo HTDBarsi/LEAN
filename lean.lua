3bdd43c08defc450b119510596685a8221de060f
HTDBarsi <57287841+HTDBarsi@users.noreply.github.com>
2022-03-04 20:01:28 +0100

    Update lean.lua

--- lean.lua
+++ lean.lua
@@ -4,5 +4,5 @@ LEAN.run = function(lean_)
     for i = 1,#lean,1 do
         lean_ = string.gsub(lean_,"LEAN".._lean[i],string.sub(lean,i,i))
     end
-    loadstring(lean_)()
+    loadstring(lean_)() 
 end
